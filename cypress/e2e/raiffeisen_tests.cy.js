4338c7d153a7ef2723c6dcaf36a73c69eebcb0e8
DmitryZhokhov <dmitryzhokhov@gmail.com>
2022-07-20 19:14:44 +0300

    update

--- cypress/e2e/raiffeisen_tests.cy.js
+++ cypress/e2e/raiffeisen_tests.cy.js
@@ -4,7 +4,7 @@ describe('Raiffeisen Tests', () => {
     cy.visit('https://www.raiffeisen.ru/')
   })
     it('Test order debit card', () => {
-    cy.step_1('+71111111111', '1111')
+    cy.step_1('+7(111)111-11-11', '1111')
     cy.step_2('Иванов Дмитрий Михайлович', '16.11.1986', 'test@gmail.com')
     cy.step_3('0505-000000','123-456','01.01.2020','1 УВД города Москвы')
     cy.step_4('г. Москва', 'г Москва, ул Октябрьская, д 2, кв 22')
