e1c405cca006d0463831860d214e475e1d157c35
DmitryZhokhov <dmitryzhokhov@gmail.com>
2022-07-21 11:53:05 +0300

    add assert gender and fullname request

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -27,23 +27,26 @@
 Cypress.Commands.add('step_1', (phoneNumber, confirmationCode) => {
     cy.check_step_header(1, 'Введите номер телефона')
     cy.get('input[name="mobilePhone"]').type(`${phoneNumber}`)
-    cy.get('div[data-step="1"] button[data-context="next"]').click()
+    cy.get('div[data-step="1"] button[data-context="next"]').should('not.be.disabled').click()
     cy.get('input[name="mobilePhoneConfirmation"]').type(`${confirmationCode}`)
 })
 Cypress.Commands.add('step_2', (fullName, birthday, email) => {
-    cy.check_step_header(3, 'Заполните контактные данные')
-    cy.get('textarea[name="name"]').type(`${fullName}{enter}`)
-    cy.wait(500)
-    cy.intercept('POST', 'Request URL: https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio', {
-    statusCode: 200,
-    body: {
-      name: fullName,
-    },
+  cy.intercept('POST', 'Request URL: https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio', {
+      statusCode: 200,
+      body: {
+          name: fullName,
+      },
   })
-    cy.get('input[name="birthday"]').type(birthday)
-    cy.get('input[placeholder="email@domain.ru"]').type(email)
-    cy.get('div[data-step="3"] button[data-context="next"]').should('not.be.disabled').click()
+  cy.check_step_header(3, 'Заполните контактные данные')
+  cy.intercept('POST', 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio').as('request')
+  cy.get('textarea[name="name"]').type(`${fullName}{enter}`)
+  cy.wait('@request')
+  cy.get('input[name="birthday"]').type(birthday)
+  cy.get('label [value="M"]').should('be.checked')
+  cy.get('input[placeholder="email@domain.ru"]').type(email)
+  cy.get('div[data-step="3"] button[data-context="next"]').should('not.be.disabled').click()
 })
+
 Cypress.Commands.add('step_3', (passportSeriaNumber, passportIssueByCode, passportIssueDate, passportIssuePlace) => {
     cy.check_step_header(4, 'Дмитрий, укажите паспортные данные')
     cy.get('input[name="passportSeriaNumber"]').type(passportSeriaNumber)
@@ -81,7 +84,7 @@ Cypress.Commands.add('step_5', (deliveryAddress) => {
         }
       }
     }).as('request')
-  cy.get('div[data-step="6"] button[data-context="next"]').click()
+  cy.get('div[data-step="6"] button[data-context="next"]').should('not.be.disabled').click()
   cy.wait('@request', {timeout: 20000}).its('request.body.auth').then((auth) => {
 
       expect(auth.phone).to.equal("71111111111")
