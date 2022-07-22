0313f510dbd5fa0d1290517560ab905a538e8e3f
DmitryZhokhov <dmitryzhokhov@gmail.com>
2022-07-22 12:43:23 +0300

    finish

--- cypress/e2e/raiffeisen_tests.cy.js
+++ cypress/e2e/raiffeisen_tests.cy.js
@@ -1,14 +1,25 @@
 describe('Raiffeisen Tests', () => {
+
   beforeEach(() => {
     cy.viewport(1280, 720)
     cy.visit('https://www.raiffeisen.ru/')
   })
-    it('Test order debit card', () => {
-    cy.step_1_phone_validation('+7(111)111-11-11', '1111')
-    cy.step_2_contact_details('Иванов Дмитрий Михайлович', 'MALE', 'M', '16.11.1986', 'test@gmail.com')
-    cy.step_3_passport_details('0505-000000','123-456','01.01.2020','1 УВД города Москвы')
-    cy.step_4_permanent_address('г. Москва', 'г Москва, ул Октябрьская, д 2, кв 22')
-    cy.step_5_delivery_details('г Москва, ул Октябрьская, д 2, кв 22')
-    cy.check_result('Заявка передана в службу доставки')
+
+    it('Order debit card by Male', () => {
+      cy.step_1_phone_validation('+7(111)111-11-11', '1111')
+      cy.step_2_contact_details('Иванов', 'Дмитрий', 'Михайлович', 'MALE', 'M', '16.11.1986', 'test@gmail.com')
+      cy.step_3_passport_details('0505-000000','123-456','01.01.2020','1 УВД города Москвы')
+      cy.step_4_permanent_address('г. Москва', 'г Москва, ул Октябрьская, д 2, кв 22')
+      cy.step_5_delivery_details('г Москва, ул Октябрьская, д 2, кв 22')
+      cy.check_result('Заявка передана в службу доставки')
+    })
+
+    it('Order debit card by Female', () => {
+      cy.step_1_phone_validation('+7(111)111-11-11', '1111')
+      cy.step_2_contact_details('Петрова', 'Елена', 'Александровна', 'FEMALE', 'F', '16.11.1986', 'test@gmail.com')
+      cy.step_3_passport_details('0505-000000','123-456','01.01.2020','1 УВД города Москвы')
+      cy.step_4_permanent_address('г. Москва', 'г Москва, ул Октябрьская, д 2, кв 22')
+      cy.step_5_delivery_details('г Москва, ул Октябрьская, д 2, кв 22')
+      cy.check_result('Заявка передана в службу доставки')
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,48 +24,56 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('step_1_phone_validation', (phoneNumber, confirmationCode) => {
-    cy.check_step_header(1, 'Введите номер телефона')
-    cy.get('input[name="mobilePhone"]').type(`${phoneNumber}`)
-    cy.get('div[data-step="1"] button[data-context="next"]').should('be.enabled').click()
-    cy.get('input[name="mobilePhoneConfirmation"]').type(`${confirmationCode}`)
+Cypress.Commands.add('check_step_header', (step) => {
+  cy.get('[data-step-hidden="2"]', {timeout: 10000}).should('have.text', ` (шаг ${step} из 5)`)
 })
-Cypress.Commands.add('step_2_contact_details', (fullName, gender1, gender2, birthday, email) => {
-  cy.check_step_header(3, 'Заполните контактные данные')
-  cy.get('textarea[name="name"]').type(`${fullName}{enter}`)
+
+Cypress.Commands.add('step_1_phone_validation', (phoneNumber, confirmationCode) => {
+  cy.check_step_header('1')
+  cy.get('input[name="mobilePhone"]').type(`${phoneNumber}`)
+  cy.get('div[data-step="1"] button[data-context="next"]').click()
+  cy.get('input[name="mobilePhoneConfirmation"]').type(`${confirmationCode}`)
+})
+
+Cypress.Commands.add('step_2_contact_details', (surname, name, patronymic, gender, genderValue, birthday, email) => {
+  cy.check_step_header('2')
+  cy.get('textarea[name="name"]').type(`${surname} ${name} ${patronymic}{enter}`)
   const staticResponse = {
     body: {
-    "suggestions":[{"value":"Иванов Дмитрий Михайлович","unrestricted_value":"Иванов Дмитрий Михайлович","data":{"surname":"Иванов","name":"Дмитрий","patronymic":"Михайлович","gender":"MALE","source":null,"qc":"0"}}]
+    "suggestions":[{"value":`${surname} ${name} ${patronymic}`,"unrestricted_value":`${surname} ${name} ${patronymic}`,"data":{"surname":`${surname}`,"name":`${name}`,"patronymic":`${patronymic}`,"gender":`${gender}`,"source":null,"qc":"0"}}]
     }
   }
-  cy.intercept('POST', 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio', staticResponse)
-  cy.intercept('POST', 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio', (req) => {
-    expect(req.body).to.include(`${gender1}`)
+  cy.intercept('POST', 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio', staticResponse).as('request')
+  cy.wait('@request', {timeout: 30000}).then((xhr) => {
+    const response = xhr.response.body.suggestions[0].data.gender;
+    expect(response).to.eq(`${gender}`)
   })
   cy.get('input[name="birthday"]').type(birthday)
   cy.get('input[placeholder="email@domain.ru"]').type(email)
-  cy.get(`input[value="${gender2}"]`).should('be.checked')
-  cy.get('div[data-step="3"] button[data-context="next"]').should('be.enabled').click()
+  cy.get(`input[value="${genderValue}"]`).should('be.checked')
+  cy.get('div[data-step="3"] button[data-context="next"]').click()
 })
+
 Cypress.Commands.add('step_3_passport_details', (passportSeriaNumber, passportIssueByCode, passportIssueDate, passportIssuePlace) => {
-    cy.check_step_header(4, 'Дмитрий, укажите паспортные данные')
-    cy.get('input[name="passportSeriaNumber"]').type(passportSeriaNumber)
-    cy.get('input[name="passportIssueByCode"]').type(passportIssueByCode)
-    cy.get('input[name="passportIssueDate"]').type(passportIssueDate)
-    cy.get('textarea[name="passportIssuePlace"]').type(passportIssuePlace)
-    cy.get('div[data-step="4"] button[data-context="next"]').should('be.enabled').click()
+  cy.check_step_header('3')
+  cy.get('input[name="passportSeriaNumber"]').type(passportSeriaNumber)
+  cy.get('input[name="passportIssueByCode"]').type(passportIssueByCode)
+  cy.get('input[name="passportIssueDate"]').type(passportIssueDate)
+  cy.get('textarea[name="passportIssuePlace"]').type(passportIssuePlace)
+  cy.get('div[data-step="4"] button[data-context="next"]').click()
 })
+
 Cypress.Commands.add('step_4_permanent_address', (birthPlace, permanentAddress) => {
-    cy.check_step_header(5, 'Дмитрий, заполните информацию')
-    cy.get('textarea[name="birthPlace"]').type(`${birthPlace}{enter}`)
-    cy.get('textarea[name="permanentAddress"]').type(`${permanentAddress}{enter}`)
-    cy.get('span').contains('Только РФ').click()
-    cy.get('div[data-step="5"] button[data-context="next"]').should('be.enabled').click()
+  cy.check_step_header('4')
+  cy.get('textarea[name="birthPlace"]').type(`${birthPlace}{enter}`)
+  cy.get('textarea[name="permanentAddress"]').type(`${permanentAddress}{enter}`)
+  cy.get('span').contains('Только РФ').click()
+  cy.get('div[data-step="5"] button[data-context="next"]').click()
 })
+
 Cypress.Commands.add('step_5_delivery_details', (deliveryAddress) => {
-  cy.check_step_header(6, 'Дмитрий, выберите удобный способполучения карты')
+  cy.check_step_header('5')
   cy.get('textarea[name="deliveryAddress"]').type(`${deliveryAddress}{enter}`)
-  cy.get('span').contains('Бесплатная доставка').click()
   cy.intercept('POST', 'https://oapi.raiffeisen.ru/api/forms/public/v1.0/forms/debit-card-single-field/66/answers', {
       body: {
         "success": true,
@@ -85,15 +93,18 @@ Cypress.Commands.add('step_5_delivery_details', (deliveryAddress) => {
         }
       }
     }).as('request')
+  cy.get('span').contains('Бесплатная доставка').click()
   cy.get('div[data-step="6"] button[data-context="next"]').should('be.enabled').click()
   cy.wait('@request', {timeout: 30000}).its('request.body.auth').then((auth) => {
       expect(auth.phone).to.equal("71111111111")
       expect(auth.token).to.equal("YD5j3gq0eyQI8xJQ/Dpx7Cas")
   })
 })
+
 Cypress.Commands.add('check_result', (text) => {
     cy.get('div[class="ccform-thx-page__row ccform-thx-page__row--last"] div[class="ccform-thx-page__title"]').contains(text).should('be.visible')
 })
-Cypress.Commands.add('check_step_header', (step, text) => {
-  cy.get(`span[data-step-visible="${step}"]`).contains(text).should('be.visible')
-})
+
+/* Cypress.Commands.add('click_next', () => {
+  cy.get('button[data-context="next"]').contains('Далее').click({force: true})
+}) */
